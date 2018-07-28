cb3a7c92a1aa17d2dcabcf554dacdc880ca11ed6
Guoyi <zhangguoyi@ledouya.com>
2018-07-28 20:18:21 +0800

    -a

---
+++ text1/src/main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	. "fmt"
+)
+
+func main() {
+	var le int = 1111
+	var float float32 = 1111
+	var b bool = true
+	var by byte = 'a'
+	var s string = "你好啊！"
+
+	//fmt.Print("你好啊！")
+	Println(le)
+	Println(float)
+	Println(b)
+	Println(by)
+	Println(s)
+}
